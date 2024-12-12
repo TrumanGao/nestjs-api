043d310e04a44833c65a819d8ed46c886edc661c
TrumanGao <59617789@qq.com>
2024-12-12 11:56:13 +0800

    add interceptors for logging, transformation, and error handling in the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,11 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AccountsModule } from './accounts/accounts.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
-import { APP_FILTER } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { CatchEverythingFilter } from './common/filter/catch-everything.filter';
+import { LoggingInterceptor } from './common/interceptor/logging.interceptor';
+import { TransformInterceptor } from './common/interceptor/transform.interceptor';
+import { ErrorsInterceptor } from './common/interceptor/errors.interceptor';
 
 @Module({
   imports: [UsersModule, AccountsModule],
@@ -15,6 +18,18 @@ import { CatchEverythingFilter } from './common/filter/catch-everything.filter';
     {
       provide: APP_FILTER,
       useClass: CatchEverythingFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TransformInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ErrorsInterceptor,
     },
   ],
 })
---
+++ src/common/interceptor/errors.interceptor.ts
@@ -0,0 +1,25 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  RequestTimeoutException,
+  BadGatewayException,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable, throwError, TimeoutError } from 'rxjs';
+import { catchError, timeout } from 'rxjs/operators';
+
+@Injectable()
+export class ErrorsInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      timeout(30000),
+      catchError((err) => {
+        if (err instanceof TimeoutError) {
+          return throwError(() => new RequestTimeoutException());
+        }
+        return throwError(() => new BadGatewayException());
+      }),
+    );
+  }
+}
