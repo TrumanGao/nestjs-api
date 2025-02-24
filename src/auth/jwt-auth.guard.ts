0ff2a6f75a9c3e3d9525a0154013dbaa9370a407
TrumanGao <59617789@qq.com>
2025-02-24 18:18:31 +0800

    feat: update JWT authentication guard and strategy to use 'JWT_APP' identifier

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -4,7 +4,7 @@ import { Reflector } from '@nestjs/core';
 import { IS_PUBLIC_KEY } from './jwt-public';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {
+export class JwtAuthGuard extends AuthGuard('JWT_APP') {
   constructor(private reflector: Reflector) {
     super();
   }
